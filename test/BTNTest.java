f542a770d228eea186ff57e16763f60a3f6c3a0e
Igor Ribeiro Barbosa Duarte <igor.ribeiro.duarte@gmail.com>
2017-05-17 15:32:15 -0300

    Adding interest test of BTN class

--- test/BTNTest.java
+++ test/BTNTest.java
@@ -21,4 +21,14 @@ public class BTNTest {
 		
 		Assert.assertTrue(Math.abs(btn1.getUpdatedUnitPrice() - 26.385798) < 1e-6);
 	}
+	
+	@Test
+    public void testInterest() {
+        btn1.setUpdatedUnitPrice(30.123456);
+        btn1.setTitleInterestRate(5.67);
+        btn1.setNumberOfMonths(6);
+    
+        Assert.assertTrue(Math.abs(btn1.getInterestFactor() - 0.02795914) < 1e-8);
+        Assert.assertTrue(Math.abs(btn1.getInterest() - 0.842225) < 1e-6);
+    }
 }
