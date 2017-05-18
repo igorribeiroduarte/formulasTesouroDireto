25075b651a657c28e62379f9d9f027fd6f42dbf1
Izabela Cardoso <cizabelacristina@gmail.com>
2017-05-18 13:11:53 -0300

    Creating test for title liquid value

---
+++ test/NTNA1Test.java
@@ -0,0 +1,22 @@
+package test;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class NTNA1Test {
+	NTNA1 ntna1;
+
+	@Before
+	public void setUp() throws Exception {
+		ntna1 = new NTNA1();
+	}
+	
+	@Test
+	public void testTitleLiquidValue() {
+		ntna1.setEmissionValue(100.15);	
+		ntna1.setDepreciationPercent(0.15);
+		
+		Assert.assertTrue(Math.abs(ntna1.getTitleLiquidValue() - 15.015) < 1e-6);
+	}
+}
