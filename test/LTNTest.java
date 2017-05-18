9530aae802c15f44e94d1b87ee3bd1b969efa505
Izabela Cardoso <cizabelacristina@gmail.com>
2017-05-18 14:26:09 -0300

    Creating test for unit price

--- test/LTNTest.java
+++ test/LTNTest.java
@@ -4,20 +4,19 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import src.BTN;
+import src.LTN;
 
 public class LTNTest {
 	LTN ltn;
 
 	@Before
 	public void setUp() throws Exception {
-		ltn = new LNT();
+		ltn = new LTN();
 	}
 	
 	@Test
 	public void testUnitPrice() {
-		ltn.setSelicRate(5.15);
-		
+		ltn.setSelicRate(5.15);	
 		ltn.calculateUnitPrice();
 		
 		Assert.assertTrue(Math.abs(ltn.getUnitPrice() - 992.817773) < 1e-6);
